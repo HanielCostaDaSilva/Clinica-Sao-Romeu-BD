1fa161a362b64ea8328e9e8e847189e8871d063c
allansmithll <allanammancio2004@gmail.com>
2023-05-17 09:45:30 -0300

    Iniciando consultas

---
+++ Scripts/Queries/queries.sql
@@ -0,0 +1,8 @@
+-- Consultas de acordo com os requitos do modelo BDR propostos
+/*
+1- Obter os pacientes que tiveram consultas com médicos da clínica, e possuam idades entre 30 e 49 anos.
+*/
+
+/*
+
+*/
