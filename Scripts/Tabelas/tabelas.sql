f27e90e10085799a137e881861b4e93e207a36ef
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2023-05-19 11:25:35 -0300

    refatorado

--- Scripts/Tabelas/tabelas.sql
+++ Scripts/Tabelas/tabelas.sql
@@ -1,13 +1,13 @@
 -- Tabelas da Base de Dados da Clínica
 CREATE TABLE PACIENTE (
-    cpf                 char(11)    NOT NULL PRIMARY KEY,
-    Func_Cadastrante    char(5)     Not Null, 
-    Nome                varChar(50) Not Null, 
-    estado_urgencia     int         Not Null,
-    Data_Nascimento     DATE        Not Null,
-    Rua                 varChar(50) Not Null, 
-    Bairro              varChar(40) Not Null, 
-    Cidade              varChar(30) Not Null 
+    cpf char(11) NOT NULL PRIMARY KEY,
+    Func_Cadastrante char(5) Not Null, 
+    Nome varChar(50) Not Null, 
+    estado_urgencia int Not Null,
+    Data_Nascimento  DATE Not Null,
+    Rua  varChar(50) Not Null, 
+    Bairro varChar(40) Not Null, 
+    Cidade varChar(30) Not Null 
 );
 
 CREATE TABLE FUNCIONARIO (
@@ -29,7 +29,7 @@ CREATE TABLE MEDICO (
 
 CREATE TABLE REMEDIO_RECEITADO (
     receitaId int Not Null,
-    descricao varChar(25) Not Null
+    descricao text Not Null
 );
 
 CREATE TABLE ESPECIALIDADE (
@@ -88,3 +88,16 @@ ALTER TABLE RECEITA ADD CONSTRAINT FK_RECEITA_3
 ALTER TABLE Numero_Telefone_Paciente ADD CONSTRAINT FK_Numero_telefone_2
     FOREIGN KEY (pacienteCPF)
     REFERENCES PACIENTE (cpf);
+
+
+Alter TABLE PACIENTE add  constraint checkEstadoUrgencia  check (estado_urgencia < 6 && estado_urgencia > 0 );
+Alter TABLE PACIENTE add  constraint checkData_Nascimento  check (Data_Nascimento < current_date);
+
+Alter TABLE FUNCIONARIO add  constraint checkData_nascimento check (Data_nascimento < current_date && age(Data_Nascimento) < interval '18 years');
+Alter TABLE FUNCIONARIO add  constraint checkData_admissao check (Data_admissao <= current_date);
+Alter TABLE FUNCIONARIO add  constraint checkSalario check (salario > 0.0);
+
+Alter TABLE ESPECIALIDADE add  constraint checkPreco_consulta check (preco_consulta >= 0.0);
+
+Alter TABLE RECEITA add  constraint checkData_Realizacao check (Data_Realizacao <= current_date);
+Alter TABLE RECEITA add  constraint checkData_Validade check (data_Validade <= current_date && age(data_Validade) <= interval '30 days');
