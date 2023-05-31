c53af8e7b0cb1acf384737661103ad15e05bd636
allansmithll <allanammancio2004@gmail.com>
2023-05-31 12:05:40 -0300

    fesfjfeoçepç

--- Scripts/Queries/queries.sql
+++ Scripts/Queries/queries.sql
@@ -2,7 +2,7 @@
 /*
 1- A clínica quer saber os pacientes mais graves, com estado de urgência maior ou igual a 4,
 e com receituário nos últimos 3 meses. Sendo assim, faça uma consulta para estes casos. 
-(Uso de operador básico)
+(Uso de operador básico de filtro)
 */
 select p.nome as nome_paciente, pre.descricao, r.data_realizacao
 from paciente p 
@@ -19,12 +19,23 @@ que possuem receitas prescritas por determinados médicos, juntamente com as des
 no ano de 2023. (uso de INNER JOIN)
 */
 select p.nome as nome_paciente, f.nome as nome_medico, 
-		e.descricao as especialidade, r.descricao as descricao_remedio
+		e.descricao as especialidade, r.nome as nome_remedio
 from paciente p
 		join receita rec on p.cpf = rec.cpfPaciente
 		join medico m on rec.matMedico = m.matricula
 		join funcionario f on f.matricula = m.matricula
 		join especialidade e on m.espid = e.id
 		join prescricao pres on rec.id = pres.idReceita
-		join remedio r on pres.idRemedio = r.id;
-where 
+		join remedio r on pres.idRemedio = r.id
+where abs(extract(year from (data_realizacao))) = 2023;
+
+/*
+3- Consulte os funcionários que possuem um salário abaixo dos 5000 reais, para que sejam bonificados
+pela direção geral.
+*/
+select * from funcionario;
+select
+from funcionario f
+	inner join medico m on f.matricula = m.matricula
+	inner join cargo c on f.idcargo = c.id
+where salario_base + percentual_bonus < 5000.00
