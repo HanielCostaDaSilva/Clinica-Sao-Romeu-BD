d0b905c0fdf7dc9e6c968deafc25a0564fa0d6e5
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-06-08 00:49:04 -0300

    Corrigindo a função

--- Scripts/Functions&Procedures/Specific/ClinicaFunctions.sql
+++ Scripts/Functions&Procedures/Specific/ClinicaFunctions.sql
@@ -1,10 +1,19 @@
 -- Função que mostra dados estatísticos
-CREATE OR REPLACE FUNCTION dadosEstatisticosClinica() RETURNS VOID AS
+CREATE OR REPLACE FUNCTION dadosEstatisticosClinica() RETURNS TABLE (
+	totalPacientes		INT,
+	totalMedicos		INT,
+	mediaBonusSal		NUMERIC(10,2),
+	pacienteMaisNovo	VARCHAR(50),
+	pacienteMaisVelho	VARCHAR(50)
+	) 
+AS
 $$
 BEGIN
-	SELECT COUNT(cpf) FROM paciente	GROUPY BY cidade;
-	SELECT MIN(data_nascimento) FROM paciente;
-	SELECT MAX(data_nascimento) FROM paciente;
+	SELECT COUNT(*) INTO totalPacientes FROM paciente;
+	SELECT COUNT(*) INTO totalMedicos FROM medico;
+	SELECT AVG(percentual_bonus) INTO mediaBonusSalario FROM funcionario;
+	SELECT nome INTO pacienteMaisNovo FROM paciente ORDER BY Data_Nascimento DESC LIMIT 1;
+	SELECT nome INTO pacienteMaisVelho FROM paciente ORDER BY Data_Nascimento LIMIT 1;
 END;
 $$
 LANGUAGE 'plpgsql';
