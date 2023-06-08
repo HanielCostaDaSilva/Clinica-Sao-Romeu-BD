3879fb68cb0561a40b88229d6f35be595a705a2f
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-06-08 00:10:06 -0300

    Criação da função dadosEstatisticosClinica

---
+++ Scripts/Functions&Procedures/Specific/ClinicaFunctions.sql
@@ -0,0 +1,10 @@
+-- Função que mostra dados estatísticos
+CREATE OR REPLACE FUNCTION dadosEstatisticosClinica() RETURNS VOID AS
+$$
+BEGIN
+	SELECT COUNT(cpf)			FROM paciente	GROUPY BY cidade;
+	SELECT MIN(data_nascimento) FROM paciente;
+	SELECT MAX(data_nascimento) FROM paciente;
+END;
+$$
+LANGUAGE 'plpgsql';
