67c40135cf271d01426c6bf15938fb8ca52b1216
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-06-08 23:24:13 -0300

    Removendo a coluna salário que já havia sido removida de funcionário

--- Scripts/Functions&Procedures/Specific/ClinicaFunctions.sql
+++ Scripts/Functions&Procedures/Specific/ClinicaFunctions.sql
@@ -2,7 +2,6 @@
 CREATE OR REPLACE FUNCTION dadosEstatisticosClinica() RETURNS TABLE (
 	totalPacientes		INT,
 	totalMedicos		INT,
-	mediaSalarioBase		NUMERIC(10,2),
 	pacienteMaisNovo	VARCHAR(50),
 	pacienteMaisVelho	VARCHAR(50)
 	) 
@@ -11,7 +10,6 @@ $$
 BEGIN
 	SELECT COUNT(*) INTO totalPacientes FROM paciente;
 	SELECT COUNT(*) INTO totalMedicos FROM medico;
-	SELECT AVG(salario_base) INTO mediaSalarioBase FROM funcionario;
 	SELECT nome INTO pacienteMaisNovo FROM paciente ORDER BY Data_Nascimento DESC LIMIT 1;
 	SELECT nome INTO pacienteMaisVelho FROM paciente ORDER BY Data_Nascimento LIMIT 1;
 	RETURN NEXT;
